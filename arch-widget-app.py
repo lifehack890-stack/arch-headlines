287a985086aec8367a58170fdcddbddd534fdde6
lifehack890-stack <lifehack890@gmail.com>
2026-07-20 12:29:44 +0900

    Update arch-widget-app.py
    fix: link click via JS title intercept, remove drag conflict

--- arch-widget-app.py
+++ arch-widget-app.py
@@ -26,66 +26,26 @@ class ArchWidget(Gtk.Window):
         self.set_skip_pager_hint(True)
         self.set_app_paintable(True)
         self.set_border_width(0)
-
         screen = self.get_screen()
         visual = screen.get_rgba_visual()
         if visual:
             self.set_visual(visual)
-
         self.connect("destroy", Gtk.main_quit)
         self.connect("draw", self._on_draw)
-
-        # Drag state
-        self._dragging = False
-        self._drag_x = 0
-        self._drag_y = 0
-        self._win_x = 40
-        self._win_y = 40
-        self.add_events(
-            Gdk.EventMask.BUTTON_PRESS_MASK |
-            Gdk.EventMask.BUTTON_RELEASE_MASK |
-            Gdk.EventMask.POINTER_MOTION_MASK
-        )
+        self.add_events(Gdk.EventMask.BUTTON_PRESS_MASK)
         self.connect("button-press-event", self._on_win_press)
-        self.connect("button-release-event", self._on_win_release)
-        self.connect("motion-notify-event", self._on_motion)
-
         settings = WebKit2.Settings()
         settings.set_enable_javascript(True)
         settings.set_allow_file_access_from_file_urls(True)
         settings.set_allow_universal_access_from_file_urls(True)
-
         self.webview = WebKit2.WebView()
         self.webview.set_settings(settings)
         self.webview.connect("notify::title", self._on_title_change)
-        self.webview.connect("decide-policy", self._on_policy)
-        self.webview.connect("context-menu", self._on_context_menu)
-
         self.add(self.webview)
         self.show_all()
-
         run_fetch()
         self.load_html()
         GLib.timeout_add_seconds(3600, self.refresh)
-
-    def _on_policy(self, webview, decision, decision_type):
-        if decision_type == WebKit2.PolicyDecisionType.NAVIGATION_ACTION:
-            action = decision.get_navigation_action()
-            uri = action.get_request().get_uri()
-            if uri.startswith("http"):
-                subprocess.Popen(["xdg-open", uri])
-                decision.ignore()
-                return True
-        return False
-
-    def _on_context_menu(self, webview, menu, event, hit):
-        # Replace context menu with quit option
-        menu.remove_all()
-        item = WebKit2.ContextMenuItem.new_from_stock_action(WebKit2.ContextMenuAction.NO_ACTION)
-        quit_item = Gtk.MenuItem(label="Quit arch-headlines")
-        quit_item.connect("activate", lambda _: Gtk.main_quit())
-        menu.append(WebKit2.ContextMenuItem.new_separator())
-        return False
 
     def _on_draw(self, widget, cr):
         cr.set_source_rgba(0, 0, 0, 0)
@@ -94,7 +54,6 @@ class ArchWidget(Gtk.Window):
 
     def _on_win_press(self, widget, event):
         if event.button == 3:
-            # Right-click quit menu
             menu = Gtk.Menu()
             item = Gtk.MenuItem(label="Quit arch-headlines")
             item.connect("activate", lambda _: Gtk.main_quit())
@@ -102,26 +61,14 @@ class ArchWidget(Gtk.Window):
             menu.show_all()
             menu.popup_at_pointer(event)
             return True
-        if event.button == 1:
-            self._dragging = True
-            self._drag_x = event.x_root
-            self._drag_y = event.y_root
-            self._win_x, self._win_y = self.get_position()
-
-    def _on_win_release(self, widget, event):
-        self._dragging = False
-
-    def _on_motion(self, widget, event):
-        if self._dragging and (event.state & Gdk.ModifierType.BUTTON1_MASK):
-            dx = event.x_root - self._drag_x
-            dy = event.y_root - self._drag_y
-            # Only move if dragged more than 5px (to allow clicks)
-            if abs(dx) > 5 or abs(dy) > 5:
-                self.move(int(self._win_x + dx), int(self._win_y + dy))
+        return False
 
     def _on_title_change(self, webview, param):
         title = webview.get_title() or ""
-        if title == "expanded":
+        if title.startswith("open:"):
+            url = title[5:]
+            subprocess.Popen(["xdg-open", url])
+        elif title == "expanded":
             self.resize(500, 700)
         elif title == "collapsed":
             self.resize(500, 88)
